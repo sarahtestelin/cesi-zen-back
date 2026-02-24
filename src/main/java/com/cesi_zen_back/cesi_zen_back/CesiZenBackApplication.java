f8b8ea57f48ce0c73e9107e0bbbb7f96c05df1e9
Sarah Testelin <sarah.khelil0709@gmail.com>
2026-02-24 15:56:02 +0100

    Initial commit

---
+++ src/main/java/com/cesi_zen_back/cesi_zen_back/CesiZenBackApplication.java
@@ -0,0 +1,13 @@
+package com.cesi_zen_back.cesi_zen_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesiZenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesiZenBackApplication.class, args);
+	}
+
+}
